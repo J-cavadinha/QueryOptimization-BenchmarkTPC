7882737dde8f8d1e980fcdf34b87f792f0d2a36b
João Cavadinha <joaomcavadinha@gmail.com>
2025-11-10 22:45:14 -0300

    10 rodadas alternadas entre Postgre e MySQL para as queries 3 e 6 do TPC-H, todos os resultados salvos.

---
+++ query6/query_q6.sql
@@ -0,0 +1,8 @@
+EXPLAIN ANALYZE
+SELECT SUM(l_extendedprice * l_discount) AS revenue
+FROM LINEITEM
+WHERE l_shipdate >= DATE '1994-01-01' -- Filtro 1 (Data)
+    AND l_shipdate < DATE '1994-01-01' + INTERVAL '1' YEAR
+    AND l_discount BETWEEN 0.06 - 0.01 AND 0.06 + 0.01 -- Filtro 2 (Numérico)
+    AND l_quantity < 24;
+-- Filtro 3 (Numérico)
